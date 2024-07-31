c67dfca2e41ec08ed219c7166897027279c696b2
nethmakannangara <nethmakannangara007@gmail.com>
2024-07-31 21:36:32 +0530

    sample_project

---
+++ login.js
@@ -0,0 +1,16 @@
+console.log("Login");
+
+openOrderPlaceSite=()=>{
+    console.log("Hiiiiii");
+    window.open("https://www.youtube.com/","_self");
+}
+
+loadAdminPage=()=>{
+    window.open("adminlogin.html","_blank")
+}
+loadPlaceOrder=()=>{
+    window.open("place-order.html","_blank");
+}
+loadMenuPage=()=>{
+    window.open("burgers-menu.html","_blank");
+}
